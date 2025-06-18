041139974f0c7b92320f146316898c3525370db8
crispin <h.c.shin.dev09@gmail.com>
2025-06-18 16:15:54 +0900

    :test_tube: Test: add user register fail test

--- src/test/kotlin/com/crispinlab/prestudyframework/application/user/UserServiceTest.kt
+++ src/test/kotlin/com/crispinlab/prestudyframework/application/user/UserServiceTest.kt
@@ -1,5 +1,8 @@
 package com.crispinlab.prestudyframework.application.user
 
+import com.crispinlab.Snowflake
+import com.crispinlab.prestudyframework.fake.UserFakeRegisterPort
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
@@ -7,10 +10,17 @@ import org.junit.jupiter.api.Test
 
 class UserServiceTest {
     private lateinit var userService: UserService
+    private lateinit var userFakeRegisterPort: UserFakeRegisterPort
+    private val snowflake = Snowflake.create(nodeId = 100)
 
     @BeforeEach
     fun setUp() {
-        userService = UserService()
+        userFakeRegisterPort = UserFakeRegisterPort()
+        userService =
+            UserService(
+                userRegisterPort = userFakeRegisterPort,
+                snowflake = snowflake
+            )
     }
 
     @Nested
@@ -23,11 +33,23 @@ class UserServiceTest {
             @DisplayName("회원 가입을 할 수 있어야 한다.")
             fun registerTest() {
                 // given
+                val request =
+                    RegisterUserUseCase.RegisterRequest(
+                        username = "test_user",
+                        password = "1234"
+                    )
 
                 // when
-                userService.registerUser()
+                val actual: RegisterUserUseCase.RegisterResponse = userService.registerUser(request)
 
                 // then
+                Assertions.assertThat(actual).isNotNull
+                Assertions.assertThat(actual.code).isEqualTo(200)
+                Assertions.assertThat(actual.message).isEqualTo("success")
+                Assertions.assertThat(userFakeRegisterPort.findBy(request.username))
+                    .isNotNull()
+                    .extracting { it!!.username }
+                    .isEqualTo(request.username)
             }
         }
     }
--- src/test/kotlin/com/crispinlab/prestudyframework/fake/UserFakeRegisterPort.kt
+++ src/test/kotlin/com/crispinlab/prestudyframework/fake/UserFakeRegisterPort.kt
@@ -1,5 +1,10 @@
 package com.crispinlab.prestudyframework.fake
 
 import com.crispinlab.prestudyframework.application.user.port.UserRegisterPort
+import com.crispinlab.prestudyframework.domain.user.User
 
-class UserFakeRegisterPort : UserRegisterPort
+class UserFakeRegisterPort : UserRegisterPort {
+    private val storage: MutableMap<Long, User> = mutableMapOf()
+
+    fun findBy(username: String): User? = storage.values.find { it.username == username }
+}
