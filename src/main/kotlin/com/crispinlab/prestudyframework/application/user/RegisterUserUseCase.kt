2b9f69a1a59b7a1eb8a84d644eb4609ee15bc405
crispin <h.c.shin.dev09@gmail.com>
2025-06-18 15:44:16 +0900

    :sparkles: Feat: add user register usecase interface

--- src/main/kotlin/com/crispinlab/prestudyframework/application/user/RegisterUserUseCase.kt
+++ src/main/kotlin/com/crispinlab/prestudyframework/application/user/RegisterUserUseCase.kt
@@ -1,5 +1,21 @@
 package com.crispinlab.prestudyframework.application.user
 
 internal interface RegisterUserUseCase {
-    fun registerUser(): Any?
+    data class RegisterRequest(
+        val username: String,
+        val password: String
+    )
+
+    class RegisterResponse private constructor(
+        val code: Int,
+        val message: String
+    ) {
+        companion object {
+            fun success(): RegisterResponse {
+                return RegisterResponse(code = 200, message = "success")
+            }
+        }
+    }
+
+    fun registerUser(request: RegisterRequest): RegisterResponse
 }
