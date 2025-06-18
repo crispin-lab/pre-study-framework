6e19c18b5c5531ac9263a6c95db433ff241b3c55
crispin <h.c.shin.dev09@gmail.com>
2025-06-18 15:12:04 +0900

    :sparkles: Feat: add log helper

---
+++ src/main/kotlin/com/crispinlab/prestudyframework/util/Log.kt
@@ -0,0 +1,37 @@
+package com.crispinlab.prestudyframework.util
+
+import com.crispinlab.prestudyframework.exception.CustomException
+import com.crispinlab.prestudyframework.exception.ErrorCode
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+internal object Log {
+    fun <T : Any> getLogger(clazz: Class<T>): Logger = LoggerFactory.getLogger(clazz)
+
+    fun <T> logging(
+        log: Logger,
+        function: (MutableMap<String, Any>) -> T?
+    ): T {
+        val logInfo: MutableMap<String, Any> = mutableMapOf()
+
+        val startedAt: Long = now()
+        logInfo["startedAt"] = startedAt
+
+        val result: T? = function.invoke(logInfo)
+
+        val endedAt: Long = now()
+        logInfo["endedAt"] = endedAt
+
+        logInfo["timeTaken"] = measureTime(startedAt, endedAt)
+
+        log.info(logInfo.toString())
+        return result ?: throw CustomException(ErrorCode.FAILED_TO_INVOKE_IN_LOG)
+    }
+
+    fun now() = System.currentTimeMillis()
+
+    private fun measureTime(
+        startedAt: Long,
+        endedAt: Long
+    ): Long = endedAt - startedAt
+}
