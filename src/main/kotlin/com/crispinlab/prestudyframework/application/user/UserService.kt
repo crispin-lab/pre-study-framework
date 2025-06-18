611084a3cb80d2a67da189cd9bd085c46147fb39
crispin <h.c.shin.dev09@gmail.com>
2025-06-18 16:15:08 +0900

    :sparkles: Feat: add user register method

--- src/main/kotlin/com/crispinlab/prestudyframework/application/user/UserService.kt
+++ src/main/kotlin/com/crispinlab/prestudyframework/application/user/UserService.kt
@@ -1,10 +1,22 @@
 package com.crispinlab.prestudyframework.application.user
 
+import com.crispinlab.Snowflake
+import com.crispinlab.prestudyframework.application.user.port.UserRegisterPort
+import com.crispinlab.prestudyframework.common.util.Log
+import org.slf4j.Logger
 import org.springframework.stereotype.Service
 
 @Service
-internal class UserService : RegisterUserUseCase {
-    override fun registerUser(): Any? {
-        return null
-    }
+internal class UserService(
+    private val snowflake: Snowflake,
+    private val userRegisterPort: UserRegisterPort
+) : RegisterUserUseCase {
+    private val logger: Logger = Log.getLogger(UserService::class.java)
+
+    override fun registerUser(
+        request: RegisterUserUseCase.RegisterRequest
+    ): RegisterUserUseCase.RegisterResponse =
+        Log.logging(logger) {
+            return@logging RegisterUserUseCase.RegisterResponse.success()
+        }
 }
