bc826283a5c2ba90ad9a5270f07ebe01c18e9d64
crispin <h.c.shin.dev09@gmail.com>
2025-06-20 18:07:29 +0900

    :heavy_plus_sign: Dependency: add jwt library

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ group = "com.crispinlab"
 version = "0.0.1-SNAPSHOT"
 val kotlinSnowflakeVersion = "1.0.1"
 val springSecurityCryptoVersion = "6.5.0"
+val nimbusJWTVersion = "10.3"
 
 java {
     toolchain {
@@ -32,6 +33,7 @@ dependencies {
     implementation(
         "org.springframework.security:spring-security-crypto:$springSecurityCryptoVersion"
     )
+    implementation("com.nimbusds:nimbus-jose-jwt:$nimbusJWTVersion")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
