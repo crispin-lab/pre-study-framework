a7651857a116b6429fccb77a9c70a7ae3b91c4a9
crispin <h.c.shin.dev09@gmail.com>
2025-06-20 16:32:58 +0900

    :sparkles: Feat: add password matches

--- src/main/kotlin/com/crispinlab/prestudyframework/application/user/PasswordEncoder.kt
+++ src/main/kotlin/com/crispinlab/prestudyframework/application/user/PasswordEncoder.kt
@@ -5,6 +5,11 @@ import org.springframework.stereotype.Component
 
 internal interface PasswordHelper {
     fun encode(rawPassword: String): String
+
+    fun matches(
+        rawPassword: CharSequence,
+        encodedPassword: String
+    ): Boolean
 }
 
 @Component
@@ -14,4 +19,9 @@ internal class BCryptPasswordHelper(
     override fun encode(rawPassword: String): String {
         return passwordEncoder.encode(rawPassword)
     }
+
+    override fun matches(
+        rawPassword: CharSequence,
+        encodedPassword: String
+    ): Boolean = passwordEncoder.matches(rawPassword, encodedPassword)
 }
--- src/test/kotlin/com/crispinlab/prestudyframework/fake/PasswordFakeHelper.kt
+++ src/test/kotlin/com/crispinlab/prestudyframework/fake/PasswordFakeHelper.kt
@@ -4,4 +4,9 @@ import com.crispinlab.prestudyframework.application.user.PasswordHelper
 
 class PasswordFakeHelper : PasswordHelper {
     override fun encode(rawPassword: String): String = rawPassword
+
+    override fun matches(
+        rawPassword: CharSequence,
+        encodedPassword: String
+    ): Boolean = rawPassword.toString() == encodedPassword
 }
