c5dd53b98be1d857b1011322703a446cd1f5337b
crispin <h.c.shin.dev09@gmail.com>
2025-06-22 15:44:53 +0900

    :white_check_mark: Test: fix failuer due to logic update

--- src/test/kotlin/com/crispinlab/prestudyframework/fake/UserFakePort.kt
+++ src/test/kotlin/com/crispinlab/prestudyframework/fake/UserFakePort.kt
@@ -9,8 +9,9 @@ class UserFakePort : UserCommandPort, UserQueryPort {
 
     override fun findBy(username: String): User? = storage.values.find { it.username == username }
 
-    override fun register(user: User) {
+    override fun register(user: User): Long {
         storage[user.id] = user
+        return user.id
     }
 
     override fun existBy(username: String): Boolean = storage.values.any { it.username == username }
