8a6d71ebeda27ca99622d68871a44d228fc764b0
crispin <h.c.shin.dev09@gmail.com>
2025-06-22 12:56:37 +0900

    :sparkles: Feat: add base entity

---
+++ src/main/kotlin/com/crispinlab/prestudyframework/adapter/BaseEntity.kt
@@ -0,0 +1,40 @@
+package com.crispinlab.prestudyframework.adapter
+
+import jakarta.persistence.Column
+import jakarta.persistence.Id
+import jakarta.persistence.MappedSuperclass
+import jakarta.persistence.PostLoad
+import jakarta.persistence.PrePersist
+import java.time.Instant
+import org.springframework.data.domain.Persistable
+
+@MappedSuperclass
+internal abstract class BaseEntity() : Persistable<Long> {
+    @Id
+    val id: Long = 0L
+
+    @Column(nullable = false)
+    val createdAt: Instant = Instant.now()
+
+    @Column(nullable = false)
+    var updatedAt: Instant = Instant.now()
+
+    @Column(nullable = false)
+    val createdBy: Long = 0L
+
+    @Column(nullable = false)
+    var updatedBy: Long = 0L
+
+    @Transient
+    private var isNew = true
+
+    override fun isNew(): Boolean = isNew
+
+    override fun getId(): Long? = id
+
+    @PrePersist
+    @PostLoad
+    private fun markNotNew() {
+        isNew = false
+    }
+}
