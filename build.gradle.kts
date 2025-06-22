4b83f2a96194bf960939367f59eb67f8097447f9
crispin <h.c.shin.dev09@gmail.com>
2025-06-22 12:56:07 +0900

    :heavy_plus_sign: Dependency: add data jpa starter

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ import org.jmailen.gradle.kotlinter.tasks.InstallPrePushHookTask
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
+    kotlin("plugin.jpa") version "1.9.25"
     id("org.jmailen.kotlinter") version "4.3.0"
     id("org.springframework.boot") version "3.5.0"
     id("io.spring.dependency-management") version "1.1.7"
@@ -34,6 +35,7 @@ dependencies {
         "org.springframework.security:spring-security-crypto:$springSecurityCryptoVersion"
     )
     implementation("com.nimbusds:nimbus-jose-jwt:$nimbusJWTVersion")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
