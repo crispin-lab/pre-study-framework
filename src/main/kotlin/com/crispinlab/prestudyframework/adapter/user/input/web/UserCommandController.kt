d9d0eac619d4beba2024535b855660bdb06e8dd7
crispin <h.c.shin.dev09@gmail.com>
2025-06-23 22:48:34 +0900

    :sparkles: Feat: add user login controller

--- src/main/kotlin/com/crispinlab/prestudyframework/adapter/user/input/web/UserCommandController.kt
+++ src/main/kotlin/com/crispinlab/prestudyframework/adapter/user/input/web/UserCommandController.kt
@@ -1,9 +1,12 @@
 package com.crispinlab.prestudyframework.adapter.user.input.web
 
+import com.crispinlab.prestudyframework.adapter.user.input.web.request.UserLoginRequest
 import com.crispinlab.prestudyframework.adapter.user.input.web.request.UserRegisterRequest
 import com.crispinlab.prestudyframework.adapter.user.input.web.response.UserResponse
 import com.crispinlab.prestudyframework.application.user.UserCommandUseCase
 import jakarta.validation.Valid
+import org.springframework.http.HttpHeaders
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -12,7 +15,8 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api")
 internal class UserCommandController(
-    private val userCommandUseCase: UserCommandUseCase
+    private val userCommandUseCase: UserCommandUseCase,
+    private val authHeaderBuilder: AuthHeaderBuilder
 ) {
     @PostMapping(
         path = ["/users"],
@@ -33,5 +37,36 @@ internal class UserCommandController(
             code = response.code,
             message = response.message
         )
+    }
+
+    @PostMapping(
+        path = ["/users/login"],
+        produces = ["application/json", "application/vnd.pre-study.com-v1+json"]
+    )
+    fun loginUser(
+        @RequestBody request: UserLoginRequest
+    ): ResponseEntity<UserResponse<Unit>> {
+        val response: UserCommandUseCase.LoginResponse =
+            userCommandUseCase.loginUser(
+                UserCommandUseCase.LoginRequest(
+                    username = request.username,
+                    password = request.password
+                )
+            )
+
+        val headers: HttpHeaders =
+            response.token?.let {
+                authHeaderBuilder.buildHeader(it)
+            } ?: HttpHeaders.EMPTY
+
+        return ResponseEntity
+            .ok()
+            .headers(headers)
+            .body(
+                UserResponse.success(
+                    code = response.code,
+                    message = response.message
+                )
+            )
     }
 }
---
+++ src/main/kotlin/com/crispinlab/prestudyframework/adapter/user/input/web/request/UserLoginRequest.kt
@@ -0,0 +1,6 @@
+package com.crispinlab.prestudyframework.adapter.user.input.web.request
+
+internal data class UserLoginRequest(
+    val username: String,
+    val password: String
+)
