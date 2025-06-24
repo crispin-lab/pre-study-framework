e5ec7d9dae9ea0b76d78eae29d498b3e8ee4fdaa
crispin <h.c.shin.dev09@gmail.com>
2025-06-24 23:45:58 +0900

    :sparkles: Feat: add article service

--- src/main/kotlin/com/crispinlab/prestudyframework/application/article/ArticleService.kt
+++ src/main/kotlin/com/crispinlab/prestudyframework/application/article/ArticleService.kt
@@ -1,9 +1,40 @@
 package com.crispinlab.prestudyframework.application.article
 
 import com.crispinlab.prestudyframework.application.article.port.ArticleQueryPort
+import com.crispinlab.prestudyframework.common.util.PageLimitCalculator
+import com.crispinlab.prestudyframework.domain.article.Article
 import org.springframework.stereotype.Service
 
 @Service
 internal class ArticleService(
     private val articleQueryPort: ArticleQueryPort
-) : ArticleQueryUseCase
+) : ArticleQueryUseCase {
+    override fun retrieveArticles(
+        params: ArticleQueryUseCase.RetrieveArticlesParams
+    ): ArticleQueryUseCase.RetrieveArticlesResponse {
+        val pageLimit: Int =
+            PageLimitCalculator.calculatePageLimit(
+                page = params.page,
+                pageSize = params.pageSize
+            )
+        val count: Int = articleQueryPort.count(pageLimit)
+
+        val articles: List<Article> =
+            articleQueryPort.retrieveAll(
+                page = params.page,
+                pageSize = params.pageSize
+            )
+
+        /*
+        todo    :: user id 리스트로 username 조회 기능 추가
+         author :: heechoel shin
+         date   :: 2025-06-24T23:34:24KST
+         */
+        articles.map { it.author }.toSet()
+
+        return ArticleQueryUseCase.RetrieveArticlesResponse(
+            articles = emptyList(),
+            count = count
+        )
+    }
+}
