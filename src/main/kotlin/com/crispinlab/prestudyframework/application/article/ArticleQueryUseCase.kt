c1f8c80fe2b924426a53b37ebabbbf31f532cad6
crispin <h.c.shin.dev09@gmail.com>
2025-06-24 23:23:58 +0900

    :sparkles: Feat: add retrieve articles response object

--- src/main/kotlin/com/crispinlab/prestudyframework/application/article/ArticleQueryUseCase.kt
+++ src/main/kotlin/com/crispinlab/prestudyframework/application/article/ArticleQueryUseCase.kt
@@ -1,3 +1,26 @@
 package com.crispinlab.prestudyframework.application.article
 
-interface ArticleQueryUseCase
+import java.time.Instant
+
+interface ArticleQueryUseCase {
+    data class RetrieveArticlesParams(
+        val page: Int,
+        val pageSize: Int
+    )
+
+    data class RetrieveArticlesResponse(
+        val articles: List<RetrieveArticleResponse>,
+        val count: Int
+    )
+
+    data class RetrieveArticleResponse(
+        val id: Long,
+        val title: String,
+        val content: String,
+        val author: String,
+        val createdAt: Instant,
+        val updatedAt: Instant
+    )
+
+    fun retrieveArticles(params: RetrieveArticlesParams): RetrieveArticlesResponse
+}
