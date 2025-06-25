58e6e26b5ffc5f6d03f62cc2acc1249300647d22
crispin <h.c.shin.dev09@gmail.com>
2025-06-25 18:04:32 +0900

    :sparkles: Feat: write article logic

--- src/main/kotlin/com/crispinlab/prestudyframework/application/article/ArticleService.kt
+++ src/main/kotlin/com/crispinlab/prestudyframework/application/article/ArticleService.kt
@@ -1,16 +1,24 @@
 package com.crispinlab.prestudyframework.application.article
 
+import com.crispinlab.prestudyframework.application.article.port.ArticleCommandPort
 import com.crispinlab.prestudyframework.application.article.port.ArticleQueryPort
+import com.crispinlab.prestudyframework.application.user.PasswordHelper
 import com.crispinlab.prestudyframework.application.user.port.UserQueryPort
+import com.crispinlab.prestudyframework.common.util.Log
 import com.crispinlab.prestudyframework.common.util.PageLimitCalculator
 import com.crispinlab.prestudyframework.domain.article.Article
+import org.slf4j.Logger
 import org.springframework.stereotype.Service
 
 @Service
 internal class ArticleService(
     private val articleQueryPort: ArticleQueryPort,
-    private val userQueryPort: UserQueryPort
-) : ArticleQueryUseCase {
+    private val articleCommandPort: ArticleCommandPort,
+    private val userQueryPort: UserQueryPort,
+    private val passwordHelper: PasswordHelper
+) : ArticleQueryUseCase, ArticleCommandUseCase {
+    private val logger: Logger = Log.getLogger(ArticleService::class.java)
+
     override fun retrieveArticles(
         params: ArticleQueryUseCase.RetrieveArticlesParams
     ): ArticleQueryUseCase.RetrieveArticlesResponse {
@@ -39,4 +47,24 @@ internal class ArticleService(
             count = count
         )
     }
+
+    override fun writeArticle(request: ArticleCommandUseCase.WriteArticleRequest) =
+        Log.logging(logger) { log ->
+            val article =
+                Article(
+                    title = request.title,
+                    content = request.content,
+                    author = request.author,
+                    password = passwordHelper.encode(request.password)
+                )
+
+            userQueryPort.findBy(request.author) ?: run {
+                log["writeFail"] = "author id: ${request.author}"
+                return@logging ArticleCommandUseCase.WriteArticleResponse.fail("invalid author")
+            }
+
+            articleCommandPort.save(article)
+
+            return@logging ArticleCommandUseCase.WriteArticleResponse.success()
+        }
 }
