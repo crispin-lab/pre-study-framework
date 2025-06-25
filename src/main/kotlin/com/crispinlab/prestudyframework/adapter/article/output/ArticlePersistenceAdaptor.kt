1e265b5ee53b6cf9e07c5c91def37f8f97bb9977
crispin <h.c.shin.dev09@gmail.com>
2025-06-25 17:52:07 +0900

    :sparkles: Feat: add article save

--- src/main/kotlin/com/crispinlab/prestudyframework/adapter/article/output/ArticlePersistenceAdaptor.kt
+++ src/main/kotlin/com/crispinlab/prestudyframework/adapter/article/output/ArticlePersistenceAdaptor.kt
@@ -1,11 +1,12 @@
 package com.crispinlab.prestudyframework.adapter.article.output
 
+import com.crispinlab.prestudyframework.application.article.port.ArticleCommandPort
 import com.crispinlab.prestudyframework.application.article.port.ArticleQueryPort
 import com.crispinlab.prestudyframework.domain.article.Article
 import org.springframework.stereotype.Component
 
 @Component
-internal class ArticlePersistenceAdaptor : ArticleQueryPort {
+internal class ArticlePersistenceAdaptor : ArticleQueryPort, ArticleCommandPort {
     override fun count(pageLimit: Int): Int {
         TODO("Not yet implemented")
     }
@@ -14,6 +15,10 @@ internal class ArticlePersistenceAdaptor : ArticleQueryPort {
         page: Int,
         pageSize: Int
     ): List<Article> {
+        TODO("Not yet implemented")
+    }
+
+    override fun save(article: Article): Long {
         TODO("Not yet implemented")
     }
 }
