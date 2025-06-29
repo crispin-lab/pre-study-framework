82fb8f77a55650602925f9b156c3ba9bbb2bc157
crispin <h.c.shin.dev09@gmail.com>
2025-06-29 22:40:26 +0900

    :sparkles: Feat: add article entity

---
+++ src/main/kotlin/com/crispinlab/prestudyframework/adapter/article/output/entity/ArticleEntity.kt
@@ -0,0 +1,30 @@
+package com.crispinlab.prestudyframework.adapter.article.output.entity
+
+import com.crispinlab.prestudyframework.adapter.BaseEntity
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Lob
+import jakarta.persistence.Table
+import java.time.Instant
+import org.hibernate.annotations.SQLDelete
+import org.hibernate.annotations.SQLRestriction
+
+@Entity
+@SQLDelete(sql = "UPDATE articles SET deleted_at = NOW(), is_deleted = true WHERE id = ?")
+@SQLRestriction("is_deleted = false")
+@Table(name = "articles")
+internal class ArticleEntity(
+    @Column(nullable = false, length = 200)
+    val title: String,
+    @Lob
+    @Column(nullable = false)
+    val content: String,
+    @Column(nullable = false)
+    val author: Long,
+    @Column(nullable = false)
+    val password: String,
+    @Column(nullable = false)
+    val isDeleted: Boolean,
+    @Column(nullable = false)
+    val deletedAt: Instant? = null
+) : BaseEntity()
