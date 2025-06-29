4e3076979e01d6f41af09ad6ddb5b5c0c96572d7
crispin <h.c.shin.dev09@gmail.com>
2025-06-29 20:24:29 +0900

    :sparkles: Feat: change article command usecase response

--- src/main/kotlin/com/crispinlab/prestudyframework/application/article/ArticleCommandUseCase.kt
+++ src/main/kotlin/com/crispinlab/prestudyframework/application/article/ArticleCommandUseCase.kt
@@ -1,6 +1,21 @@
 package com.crispinlab.prestudyframework.application.article
 
 internal interface ArticleCommandUseCase {
+    class Response private constructor(
+        val code: Int,
+        val message: String
+    ) {
+        companion object {
+            fun success(): Response {
+                return Response(code = 200, message = "success")
+            }
+
+            fun fail(message: String): Response {
+                return Response(code = 400, message = message)
+            }
+        }
+    }
+
     data class WriteArticleRequest(
         val title: String,
         val content: String,
@@ -8,20 +23,13 @@ internal interface ArticleCommandUseCase {
         val password: String
     )
 
-    class WriteArticleResponse private constructor(
-        val code: Int,
-        val message: String
-    ) {
-        companion object {
-            fun success(): WriteArticleResponse {
-                return WriteArticleResponse(code = 200, message = "success")
-            }
+    data class UpdateArticleRequest(
+        val id: Long,
+        val title: String,
+        val content: String
+    )
 
-            fun fail(message: String): WriteArticleResponse {
-                return WriteArticleResponse(code = 300, message = message)
-            }
-        }
-    }
+    fun writeArticle(request: WriteArticleRequest): Response
 
-    fun writeArticle(request: WriteArticleRequest): WriteArticleResponse
+    fun updateArticle(request: UpdateArticleRequest): Response
 }
