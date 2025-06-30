834a7dcb59d2c564db6b45bb0b98de86e5f7c5cb
crispin <h.c.shin.dev09@gmail.com>
2025-06-30 23:44:04 +0900

    :wrench: Config: add jwt filter registration config

---
+++ src/main/kotlin/com/crispinlab/prestudyframework/common/config/FilterConfig.kt
@@ -0,0 +1,18 @@
+package com.crispinlab.prestudyframework.common.config
+
+import com.crispinlab.prestudyframework.adapter.article.input.filter.JWTFilter
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class FilterConfig {
+    @Bean
+    fun jwtFilterRegistration(jwtFilter: JWTFilter): FilterRegistrationBean<JWTFilter> {
+        val filterRegistrationBean: FilterRegistrationBean<JWTFilter> =
+            FilterRegistrationBean(jwtFilter)
+        filterRegistrationBean.addUrlPatterns("/api/article/*")
+        filterRegistrationBean.order = 1
+        return filterRegistrationBean
+    }
+}
