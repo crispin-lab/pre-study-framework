0d0644550a2126f44c45d3f06f646bb58e7cd9d8
crispin <h.c.shin.dev09@gmail.com>
2025-07-02 16:22:50 +0900

    :sparkles: Feat: update error code

--- src/main/kotlin/com/crispinlab/prestudyframework/common/exception/ErrorCode.kt
+++ src/main/kotlin/com/crispinlab/prestudyframework/common/exception/ErrorCode.kt
@@ -12,5 +12,6 @@ internal enum class ErrorCode(
     FAILED_TO_INVOKE_IN_LOG(code = 100, message = "failed to invoke in log."),
     INVALID_REQUEST_VALUE(code = 101, message = "invalid request value."),
     MISSING_JWT_COOKIE(code = 102, message = "missing jwt cookie"),
-    INVALID_JWT(code = 103, message = "invalid jwt")
+    INVALID_JWT(code = 103, message = "invalid jwt"),
+    NOT_FOUND_SUBJECT(code = 104, message = "not found subject")
 }
