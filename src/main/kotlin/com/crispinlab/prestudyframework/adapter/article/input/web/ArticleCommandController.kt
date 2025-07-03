8ccf5e46646689045d6578873a7e0ad3476d20d7
crispin <h.c.shin.dev09@gmail.com>
2025-07-03 09:17:10 +0900

    :sparkles: Feat: add update article controller

--- src/main/kotlin/com/crispinlab/prestudyframework/adapter/article/input/web/ArticleCommandController.kt
+++ src/main/kotlin/com/crispinlab/prestudyframework/adapter/article/input/web/ArticleCommandController.kt
@@ -1,11 +1,15 @@
 package com.crispinlab.prestudyframework.adapter.article.input.web
 
+import com.crispinlab.prestudyframework.adapter.article.input.web.extensions.getUserIdOrNull
+import com.crispinlab.prestudyframework.adapter.article.input.web.request.UpdateArticleRequest
 import com.crispinlab.prestudyframework.adapter.article.input.web.request.WriteArticleRequest
 import com.crispinlab.prestudyframework.adapter.article.input.web.response.ArticleResponse
 import com.crispinlab.prestudyframework.application.article.ArticleCommandUseCase
 import com.crispinlab.prestudyframework.common.exception.ErrorCode
-import jakarta.servlet.ServletRequest
+import jakarta.servlet.http.HttpServletRequest
 import jakarta.validation.Valid
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -22,10 +26,10 @@ internal class ArticleCommandController(
     )
     fun writeArticle(
         @RequestBody @Valid request: WriteArticleRequest,
-        servletRequest: ServletRequest
+        servletRequest: HttpServletRequest
     ): ArticleResponse<Unit> {
         val userId: Long =
-            servletRequest.getAttribute("userId") as? Long
+            servletRequest.getUserIdOrNull()
                 ?: return ArticleResponse.error(
                     code = ErrorCode.NOT_FOUND_SUBJECT.code,
                     message = ErrorCode.NOT_FOUND_SUBJECT.message
@@ -34,6 +38,39 @@ internal class ArticleCommandController(
         val response: ArticleCommandUseCase.Response =
             articleCommandUseCase.writeArticle(
                 ArticleCommandUseCase.WriteArticleRequest(
+                    title = request.title,
+                    content = request.content,
+                    author = userId,
+                    password = request.password
+                )
+            )
+
+        return ArticleResponse.success(
+            code = response.code,
+            message = response.message
+        )
+    }
+
+    @PatchMapping(
+        path = ["/articles/{id}"],
+        produces = ["application/json", "application/vnd.pre-study.com-v1+json"]
+    )
+    fun updateArticle(
+        @RequestBody @Valid request: UpdateArticleRequest,
+        @PathVariable(required = true) id: Long,
+        servletRequest: HttpServletRequest
+    ): ArticleResponse<Unit> {
+        val userId: Long =
+            servletRequest.getUserIdOrNull()
+                ?: return ArticleResponse.error(
+                    code = ErrorCode.NOT_FOUND_SUBJECT.code,
+                    message = ErrorCode.NOT_FOUND_SUBJECT.message
+                )
+
+        val response: ArticleCommandUseCase.Response =
+            articleCommandUseCase.updateArticle(
+                ArticleCommandUseCase.UpdateArticleRequest(
+                    id = id,
                     title = request.title,
                     content = request.content,
                     author = userId,
---
+++ src/main/kotlin/com/crispinlab/prestudyframework/adapter/article/input/web/request/UpdateArticleRequest.kt
@@ -0,0 +1,14 @@
+package com.crispinlab.prestudyframework.adapter.article.input.web.request
+
+import com.crispinlab.prestudyframework.common.annotation.NotEmptyOrBlank
+import com.crispinlab.prestudyframework.common.annotation.Password
+import com.crispinlab.prestudyframework.common.annotation.Title
+
+data class UpdateArticleRequest(
+    @Title
+    val title: String,
+    @NotEmptyOrBlank
+    val content: String,
+    @Password
+    val password: String
+)
