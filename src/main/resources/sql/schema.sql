0bd9157312aad4105931b724dc024a466f06f195
crispin <h.c.shin.dev09@gmail.com>
2025-07-03 11:02:27 +0900

    :sparkles: Feat: add article table ddl

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,4 +1,5 @@
 drop table if exists users cascade;
+drop table if exists articles cascade;
 
 create table users
 (
@@ -10,4 +11,20 @@ create table users
   password   varchar(64)                 not null,
   username   varchar(20)                 not null,
   primary key (id)
-)
+);
+
+create table articles
+(
+  is_deleted boolean                     not null,
+  author     bigint                      not null,
+  created_at timestamp(6) with time zone not null,
+  created_by bigint                      not null,
+  deleted_at timestamp(6) with time zone not null,
+  id         bigint                      not null,
+  updated_at timestamp(6) with time zone not null,
+  updated_by bigint                      not null,
+  title      varchar(200)                not null,
+  password   varchar(255)                not null,
+  content    clob                        not null,
+  primary key (id)
+);
